e403f2cadc8a53ed059f34336628b27742ec5130
amarnadh meda <amaranadh.m@westagilelabs.com>
2024-01-29 13:13:03 +0530

    Add Faa config changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import Login from './login';
 import Home from './home';
+const history = createBrowserHistory({ basename: '/faa' });
 
 function App() {
   return (
-    <Router>
+    <Router history={history} basename="/faa">
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
