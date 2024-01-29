61203add9ebce64cb2579dc06e69a03effbb63b4
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-29 21:02:56 +0530

    if token is there then we are redirect to home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login';
 import Home from './home';
+import { getToken } from "./utils";
 
 function App() {
+
+  const token=getToken()
   return (
     <Router>
     <Routes>
+    {token ?
+      <Route path="/" element={<Home />} />
+      :
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+    }
+   <Route path="/home" element={<Home />} />
     </Routes>
   </Router>
   );
